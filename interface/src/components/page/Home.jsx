3be90a547d86333238e8cd519c6f994996d95465
muhamash <muhammad-ashraful@outlook.com>
2024-05-22 05:24:46 +0600

    will see next day

--- interface/src/components/page/Home.jsx
+++ interface/src/components/page/Home.jsx
@@ -7,8 +7,6 @@ import Stagger from '../stagger/Stagger';
 
 export default function Home ({data})
 {
-   
-    
     return (
         <div className="flex justify-center items-center flex-col gap-10 py-10">
             <p className="text-xl">This is a Home Page</p>
