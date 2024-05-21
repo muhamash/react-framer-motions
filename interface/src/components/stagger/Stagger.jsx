15a515a33895d35868e81bc8b913103c9107a4bc
muhamash <muhammad-ashraful@outlook.com>
2024-05-21 19:01:37 +0600

    update

--- interface/src/components/stagger/Stagger.jsx
+++ interface/src/components/stagger/Stagger.jsx
@@ -1,46 +1,47 @@
-import { motion, stagger, useAnimate } from "framer-motion";
-import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import useWrapperAnimation from "../hooks/useWrapperAnimation";
 
-const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
+// const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
 
-function useMenuAnimation(isOpen) {
-  const [scope, animate] = useAnimate();
+// function useMenuAnimation(isOpen) {
+//   const [scope, animate] = useAnimate();
 
-  useEffect(() => {
-    animate(".arrow", { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
+//   useEffect(() => {
+//     animate(".arrow", { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
 
-    animate(
-      "ul",
-      {
-        clipPath: isOpen
-          ? "inset(0% 0% 0% 0% round 10px)"
-          : "inset(10% 50% 90% 50% round 10px)",
-      },
-      {
-        type: "spring",
-        bounce: 0,
-        duration: 0.5,
-      }
-    );
+//     animate(
+//       "ul",
+//       {
+//         clipPath: isOpen
+//           ? "inset(0% 0% 0% 0% round 10px)"
+//           : "inset(10% 50% 90% 50% round 10px)",
+//       },
+//       {
+//         type: "spring",
+//         bounce: 0,
+//         duration: 0.5,
+//       }
+//     );
 
-    animate(
-      "li",
-      isOpen
-        ? { opacity: 1, scale: 1, filter: "blur(0px)" }
-        : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
-      {
-        duration: 0.2,
-        delay: isOpen ? staggerMenuItems : 0,
-      }
-    );
-  }, [isOpen]);
+//     animate(
+//       "li",
+//       isOpen
+//         ? { opacity: 1, scale: 1, filter: "blur(0px)" }
+//         : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
+//       {
+//         duration: 0.2,
+//         delay: isOpen ? staggerMenuItems : 0,
+//       }
+//     );
+//   }, [isOpen]);
 
-  return scope;
-}
+//   return scope;
+// }
 
-export default function App() {
+export default function Stagger() {
   const [isOpen, setIsOpen] = useState(false);
-  const scope = useMenuAnimation(isOpen);
+  const scope = useWrapperAnimation(isOpen);
 
   return (
     <nav className="bg-cyan-500 shadow-lg" ref={scope}>
--- server/server.js
+++ server/server.js
@@ -2,24 +2,30 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 4001;
+const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello, welcome to the stagger server!');
-});
+app.get( '/', ( req, res ) =>
+{
+  res.send( 'Hello, welcome to the stagger server!' );
+} );
 
-app.get('/image', (req, res) => {
-  const filePath = path.join(__dirname, './database/data.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Error reading data file');
+app.get( '/image', ( req, res ) =>
+{
+  const filePath = path.join( __dirname, './database/data.json' );
+  fs.readFile( filePath, 'utf8', ( err, data ) =>
+  {
+    if ( err )
+    {
+      res.status( 500 ).send( 'Error reading data file' );
       return;
     }
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-  });
-});
+    res.setHeader( 'Content-Type', 'application/json' );
+    res.send( data );
+  } );
+} );
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen( port, () =>
+{
+  console.log( `Server is running at http://localhost:${port}` );
+  console.log( `Server is responding with data at http://localhost:${port}/image` );
+} );
