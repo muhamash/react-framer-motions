c890befb30d1c73268e444d9cf23b1036695d54d
muhamash <muhammad-ashraful@outlook.com>
2024-05-24 22:59:25 +0600

    lazyload

---
+++ interface/src/components/utils/LazyLoad.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import React from 'react';
+
+export default function LazyImport(file) {
+    return React.lazy( () => import( `../components/${file}` )
+        .then( ( module ) =>
+        {
+            if ( module && module.default )
+            {
+                console.log('i am loading lazily!!!')
+                return module;
+            } else
+            {
+                throw new Error( `Module ${file} does not have a default export.` );
+            }
+        } )
+        .catch( ( error ) =>
+        {
+            console.error( 'Error loading component:', error );
+            throw error;
+        } )
+    );
+}
